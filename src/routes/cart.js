79046b0cd8e76ec8fd99eea95d8d10933281a1df
RifqiMalik29 <malikiskandar29@gmail.com>
2021-01-10 08:54:39 +0700

    add authentication into cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,8 +4,8 @@ const { authentication } = require("../middlewares/auth");
 
 router
   .post("/", authentication, cartController.addToCart)
-  .delete("/:id", cartController.deleteFromCart)
-  .patch("/:id", cartController.updateChecklist)
+  .delete("/:id", authentication, cartController.deleteFromCart)
+  .patch("/:id", authentication, cartController.updateChecklist)
   .get("/all", authentication, cartController.showAllCart)
 
 module.exports = router;
